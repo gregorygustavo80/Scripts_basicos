1f012e91815660dea48b6579073d8c2c2e5c0f2c
gregorygustavo80 <gregorygustavo80@gmail.com>
2024-05-08 08:25:37 -0300

    Update

--- numero_par.py
+++ numero_par.py
@@ -1,8 +1,7 @@
 while True
     numero = int(input('digite um número para verificar se é par: '))
     if numero % 2 == 0:
-        print(f'{numero} é par')
-        
+        print(f'{numero} é par')   
     else:
         print(f'{numero} é impar')
         
