8a7857ed1752c4ab0e53e1fd766f9b3b2c9e736e
gregorygustavo80 <gregorygustavo80@gmail.com>
2024-05-25 17:34:06 -0300

    update

--- Sorteio.py
+++ Sorteio.py
@@ -1,7 +1,21 @@
-import random 
-lista = ['Fulano', 'Ciclano', 'Beltrano']
+import random
 
-sorteado = random.sample(lista, 1)
+nomes_participantes = []
+entrada_nomes = input("Digite os nomes separados por vírgula: ")
 
-for i in sorteado:
-    print(f'O sorteado foi {i}')
+if entrada_nomes.strip() == '':
+    pass
+else:
+    nomes_participantes.extend([nome.strip() for nome in entrada_nomes.split(',')])
+
+
+if not nomes_participantes:
+    print("Nenhum nome foi fornecido.")
+else:
+    confirmacao_sorteio = input('Digite [S] para sortear: ')
+    
+    if confirmacao_sorteio.upper() == 'S' or confirmacao_sorteio.upper == 's':
+        sorteado = random.choice(nomes_participantes)
+        print(f'O sorteado foi {sorteado}')
+    else:
+        print("O sorteio não foi realizado.")
