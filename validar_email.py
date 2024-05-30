9ec326e6fbaca4e3faf8d3f4144abe865e7e2d2a
gregorygustavo80 <gregorygustavo80@gmail.com>
2024-05-30 15:46:33 -0300

    update

--- validar_email.py
+++ validar_email.py
@@ -3,6 +3,6 @@ import re
 email = input("Qual o seu e-mail? ").strip()
 
 if re.search(r"^\w+@(\w+\.)?\w+\.(edu|com|org|net)$", email, re.IGNORECASE):
-    print("Valid")
+    print("Válido")
 else:
-    print("Invalid")
+    print("Inválido")
