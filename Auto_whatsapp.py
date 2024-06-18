fcf55ff085916f6302dc27e88bb34154afd1b9c2
gregorygustavo80 <gregorygustavo@hotmail.com>
2024-06-18 01:43:32 +0000

    update

--- Auto_whatsapp.py
+++ Auto_whatsapp.py
@@ -1,17 +1,19 @@
 import sys
 import subprocess
+import pkg_resources
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-required_packages = ['pywhatkit']
+required_packages = ['opencv-python', 'mediapipe', 'pyautogui', 'pycaw', 'comtypes'] 
 
+installed_packages = {pkg.key for pkg in pkg_resources.working_set}
 for package in required_packages:
-    try:
-        __import__(package)
-    except ImportError:
+    if package not in installed_packages:
         print(f"Instalando {package}...")
         install(package)
+
+        
 
 import pywhatkit as kit
 
